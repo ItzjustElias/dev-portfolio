1855b766931439f15a6f8a3b5af924c059a8731e
Elias <hologamer7788@gmail.com>
2024-11-02 09:20:33 +0100

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,18 @@
+// index.jsx
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-ReactDOM.render(
+// Select the root element where the React app will be rendered
+const container = document.getElementById('root');
+
+// Create a root using React 18's createRoot
+const root = ReactDOM.createRoot(container);
+
+// Render the App component within the root
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
